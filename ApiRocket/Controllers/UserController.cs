ab189968b41f37f75489b510487f582c6e41aa02
fat83dotcom <fernando.quimic@gmail.com>
2025-02-13 17:42:51 -0300

    Commit inicial.

---
+++ ApiRocket/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiRocket.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
