3e3f01905000e9384599405deda6c25d8a9af632
fat83dotcom <fernando.quimic@gmail.com>
2025-02-14 19:02:12 -0300

    Adiciona classe RequestUpdateUser com formatação automática do nome
    - Implementa a classe `RequestUpdateUser` que herda de `User`:
      - Adiciona a propriedade `Name` com um setter que converte o valor para maiúsculas automaticamente ao ser atribuído.
      - Garante consistência na formatação dos nomes armazenados.
    
    Essa alteração visa padronizar a formatação dos nomes de usuário durante atualizações, melhorando a consistência dos dados.

---
+++ ApiRocket/Comunications/Requests/RequestUpdateUser.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiRocket.Comunications.Requests
+{
+    public class RequestUpdateUser : User
+    {
+        private string? _name;
+        public new string? Name  
+        {
+            get => _name;
+            set => _name = value?.ToUpper(); 
+        }
+    }
+}
