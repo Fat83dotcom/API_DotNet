da8503e37f924bde4ed06f7c73b3393e829c92d0
fat83dotcom <fernando.quimic@gmail.com>
2025-02-14 18:59:20 -0300

    - Cria a classe `RequestGetUser` que herda de `User` e inclui:   - Propriedade `Id` do tipo `Guid` com valor padrão gerado automaticamente.   - Propriedade `Name` que converte o valor para maiúsculas ao ser atribuído, utilizando a cultura atual.
    - Implementa a classe `BadRequestResponse` que herda de `ObjectResult` para respostas personalizadas:
      - Propriedades `Title` e `Detail` para detalhamento do erro.
      - Propriedade `StatusCode` fixa com valor 400 (Bad Request).
    
    Essas alterações visam melhorar a manipulação de requisições e respostas na API, garantindo consistência e clareza nas mensagens de erro."

---
+++ ApiRocket/Comunications/Requests/RequestGetUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiRocket.Comunications.Requests
+{
+    public class RequestGetUser : User
+    {
+        public new Guid Id { get; set; } = Guid.NewGuid();
+        private string? _name;
+        public new string? Name
+        {
+            get => _name;
+            set => _name = value?.ToUpper(System.Globalization.CultureInfo.CurrentCulture);
+        }
+    }
+
+    public class BadRequestResponse(object? value) : ObjectResult(value)
+    {
+        public string? Title { get; set; }
+        public string? Detail { get; set; }
+        public new int StatusCode { get; } = 400;
+    }
+}
