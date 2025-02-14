8479d62751ddbdc98503df82d864035fb74be2f0
fat83dotcom <fernando.quimic@gmail.com>
2025-02-14 19:00:44 -0300

    "Adiciona classe RequestRegisterUserJson com validação de registro inválido
    - Implementa a classe `RequestRegisterUserJson` que herda de `User`:
      - Adiciona o método `InvalidRegister()` para validar se os campos obrigatórios (`Name`, `Email` e `Password`) estão vazios ou nulos.
      - Retorna `true` se algum dos campos estiver inválido, caso contrário, retorna `false`.
    
    Essa alteração visa garantir a validação básica dos dados de registro de usuário, evitando registros incompletos ou inconsistentes."

---
+++ ApiRocket/Comunications/Requests/RequestRegisterUserJson.cs
@@ -0,0 +1,19 @@
+﻿namespace ApiRocket.Comunications.Requests
+{
+    public class RequestRegisterUserJson : User
+    {
+
+        public bool InvalidRegister()
+        {
+            if (
+                    string.IsNullOrEmpty(Name)
+                    || string.IsNullOrEmpty(Email)
+                    || string.IsNullOrEmpty(Password)
+                )
+            {
+                return true;
+            }
+            return false;   
+        }
+    }
+}
