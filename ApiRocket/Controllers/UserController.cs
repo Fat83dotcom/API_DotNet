c2cc01dafc399429d89bae500fce0f2c699ea33e
fat83dotcom <fernando.quimic@gmail.com>
2025-02-13 21:58:39 -0300

    feat(UserController): Adiciona suporte para filtragem de usuários por nome e idade
    - Modifica o método `Get` no `UserController` para aceitar parâmetros opcionais `name` e `age`.
    - Adiciona validação para garantir que ambos os parâmetros `name` e `age` sejam fornecidos.
    - Implementa a lógica para validar se `age` é um número inteiro válido.
    - Retorna um objeto `Response` com os dados do usuário se a validação for bem-sucedida.
    - Adiciona respostas de documentação com `ProducesResponseType` para melhorar a clareza da API.
    
    Esta alteração permite que a API filtre usuários com base no nome e idade, melhorando a flexibilidade e usabilidade do endpoint. As validações adicionadas garantem que os dados recebidos sejam consistentes e válidos, evitando erros durante o processamento.

--- ApiRocket/Controllers/UserController.cs
+++ ApiRocket/Controllers/UserController.cs
@@ -8,9 +8,20 @@ namespace ApiRocket.Controllers
     public class UserController : ControllerBase
     {
         [HttpGet]
-        public IActionResult Get()
+        [ProducesResponseType(typeof(Response), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Get(string? name, string? age)
         {
-            return Ok();
+            if (string.IsNullOrEmpty(name) || string.IsNullOrEmpty(age))
+            {
+                return BadRequest();
+            }
+            if (int.TryParse(age, out int _))
+            {
+                Response response = new(name, int.Parse(age));
+                return Ok(response); 
+            }
+            return BadRequest();
         }
     }
 }
