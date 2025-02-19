95f7346f38fab57ee7109814e6dbe3fc1a568245
Fernando <fernando@tsplus.com.br>
2025-02-18 21:35:21 -0300

    Request para o pet

---
+++ Petfolio.Communication/Requests/RequestRegisterPetJson.cs
@@ -0,0 +1,11 @@
+﻿using Petfolio.Communication.Enum;
+
+namespace Petfolio.Communication.Requests
+{
+    public class RequestRegisterPetJson
+    {
+        public required string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public PetType Type { get; set; }
+    }
+}
