db27def5df7dd78eb9db8f72ead0bc89c75faa4d
Fat83dotcom <fernando.quimic@gmail.com>
2025-02-24 16:41:30 -0300

    user case para deletar um pet por id

---
+++ PetFolio/UseCase/Pet/Delete/DeletePetUserCase.cs
@@ -0,0 +1,10 @@
+﻿namespace Petfolio.API.UseCase.Pet.Delete
+{
+    public class DeletePetUserCase
+    {
+        public void Execute(int id)
+        {
+            // logca para deletar no banco
+        }
+    }
+}
