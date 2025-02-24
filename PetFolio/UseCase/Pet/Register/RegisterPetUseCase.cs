28267860221edd713582922f260e0798e2321b10
Fat83dotcom <fernando.quimic@gmail.com>
2025-02-23 21:30:30 -0300

    Use casse para resgistar os pets.

---
+++ PetFolio/UseCase/Pet/Register/RegisterPetUseCase.cs
@@ -0,0 +1,17 @@
+﻿using Petfolio.Communication.Requests;
+using Petfolio.Communication.Responses;
+
+namespace Petfolio.API.UseCase.Pet.Register
+{
+    public class RegisterPetUseCase
+    {
+        public ResponseRegisterPetJson Execute(RequestPetJson request)
+        {
+            return new ResponseRegisterPetJson
+            {
+                Id = 2,
+                Name = request.Name,
+            };
+        }
+    }
+}
