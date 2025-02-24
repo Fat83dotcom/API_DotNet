11207d02a7c845f0e5bf9a31c8af1cbdf5b557e0
Fat83dotcom <fernando.quimic@gmail.com>
2025-02-23 21:29:25 -0300

    Use case para buscar todos os pets

---
+++ PetFolio/UseCase/Pet/GetAll/GetAllPetsUseCase.cs
@@ -0,0 +1,29 @@
+﻿using Petfolio.Communication.Responses;
+
+namespace Petfolio.API.UseCase.Pet.GetAll
+{
+    public class GetAllPetsUseCase
+    {
+        public ResponseAllPetsJson Execute()
+        {
+            return new ResponseAllPetsJson
+            {
+                Pets =
+                [
+                    new ResponseShortPetJson
+                    {
+                        Id = 1,
+                        Name = "Mantega",
+                        Type = Communication.Enum.PetType.Cat
+                    },
+                    new ResponseShortPetJson
+                    {
+                        Id= 2,
+                        Name = "Boinho",
+                        Type = Communication.Enum.PetType.Cat
+                    }
+                ]
+            };
+        }
+    }
+}
