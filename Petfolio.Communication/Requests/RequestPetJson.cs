089d483f3f14c713845ff9b22123a42b727c6b75
Fat83dotcom <fernando.quimic@gmail.com>
2025-02-23 21:31:41 -0300

    Classe que define o request

---
+++ Petfolio.Communication/Requests/RequestPetJson.cs
@@ -0,0 +1,11 @@
+﻿using Petfolio.Communication.Enum;
+
+namespace Petfolio.Communication.Requests
+{
+    public class RequestPetJson
+    {
+        public required string Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public PetType Type { get; set; }
+    }
+}
