b4e23bc4de33d0e0a5a88d0c09c41bd855781688
Fat83dotcom <fernando.quimic@gmail.com>
2025-02-24 16:41:58 -0300

    use case para buscar um pet por id

---
+++ PetFolio/UseCase/Pet/GetById/GetByIdPetUseCase.cs
@@ -0,0 +1,28 @@
+﻿using Petfolio.Communication.Responses;
+
+namespace Petfolio.API.UseCase.Pet.GetById
+{
+    public class GetByIdPetUseCase
+    {
+        public ResponseGetByIdPetJson Execute(int id)
+        {
+            if (id == 2)
+            {
+                return new ResponseGetByIdPetJson
+                {
+                    Id = -1,
+                    Name = "",
+                    Type = Communication.Enum.PetType.Cat
+                };
+
+            }
+            return new ResponseGetByIdPetJson
+            {
+                Id = id,
+                Name = "Manteguinha",
+                Birthday = new(2017, 8, 23),
+                Type = Communication.Enum.PetType.Cat
+            };
+        }
+    }
+}
