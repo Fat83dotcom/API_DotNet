d2403272de12b1d617eed07d43661959c670d0d6
Fat83dotcom <fernando.quimic@gmail.com>
2025-02-23 21:31:01 -0300

    Use case para atualizar os pets.

---
+++ PetFolio/UseCase/Pet/Update/UpdatePetUseCase.cs
@@ -0,0 +1,13 @@
+﻿using Petfolio.Communication.Requests;
+using Petfolio.Communication.Responses;
+
+namespace Petfolio.API.UseCase.Pet.Update
+{
+    public class UpdatePetUseCase
+    {
+        public void Execute(int id, RequestPetJson request)
+        {
+            
+        }
+    }
+}
